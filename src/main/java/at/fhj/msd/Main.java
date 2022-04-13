20dfb83d86b2220ca8b10491ee2de40062ec954f
AllyssaUlz <allyssa.ulz@edu.fh-joanneum.at>
2022-04-13 15:07:19 +0200

    update2

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,2 +1,15 @@
-package at.fhj.msd;public class Main {
+package at.fhj.msd;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+
+        System.out.println(cal.add(1.7,2.9));
+        System.out.println(cal.divide(4,2));
+        System.out.println(cal.minus(5.6,1));
+        System.out.println(cal.multiply(3.5, 2.9));
+
+
+    }
 }
